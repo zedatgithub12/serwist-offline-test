fb79ff75994b277553beac3247d4d5391eaedd48
zedatgithub12 <96112790+zedatgithub12@users.noreply.github.com>
2025-02-10 09:20:01 +0300

    added splash screen to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import PaymentCodeForm from "@/components/PaymentCodeForm";
 import Splash from "@/components/Splash";
-import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [splash, setSplash] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setSplash(false), 1200);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
     <main>
-      <Splash />
+      {splash ? (
+        <Splash />
+      ) : (
+        <div className=" w-full h-screen flex items-center justify-center">
+          <p className="text-4xl ">👋 Hello I'm Home page</p>
+        </div>
+      )}
     </main>
   );
 }
